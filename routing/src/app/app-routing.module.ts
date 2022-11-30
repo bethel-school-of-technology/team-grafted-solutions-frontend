396125c19504ff0f21052e1f3a0ac875f5f3fba3
91ap <101858100+91ap@users.noreply.github.com>
2022-11-30 01:33:04 -0600

    "del"

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'friends',
+    loadChildren: () => import('./friends/friends.module').then( m => m.FriendsPageModule)
   }
 ];
 @NgModule({
