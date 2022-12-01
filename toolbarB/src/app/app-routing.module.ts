2a9ea69830a4de371d2c8d7f339ee13e2eaebe81
Randy Beebe <randybb27@gmail.com>
2022-11-30 21:30:29 -0600

    "Login html created, login.ts file updated"

--- toolbarB/src/app/app-routing.module.ts
+++ toolbarB/src/app/app-routing.module.ts
@@ -29,6 +29,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./music/music.module').then((m) => m.MusicPageModule),
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+
 ];
 
 @NgModule({
