8a89d3dc93f22657fcca4c52355ad2be0b5526f8
91ap <101858100+91ap@users.noreply.github.com>
2022-12-01 18:44:26 -0600

    "updating login page"

--- toolbarB/src/app/login/LoginPageRoutingModule.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { routes } from "./login-routing.module";
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
-})
-export class LoginPageRoutingModule {}
