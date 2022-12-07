19ec6c7203149819ccba7d437c2a459b4b22b6af
91ap <101858100+91ap@users.noreply.github.com>
2022-12-07 00:45:38 -0600

    "minor adjustments to friends page and updated profile page"

--- routing/src/app/shared/profile1/profile1.page.ts
+++ routing/src/app/shared/profile1/profile1.page.ts
@@ -1,14 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-profile1',
   templateUrl: './profile1.page.html',
   styleUrls: ['./profile1.page.scss'],
 })
 export class Profile1Page implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
   isModalOpen = false;
 
   setOpen(isOpen: boolean) {
@@ -17,4 +14,55 @@ export class Profile1Page implements OnInit {
   setClose(isOpen: boolean) {
     this.isModalOpen = !isOpen;
   }
+
+  constructor(private alertController: AlertController) {}
+
+  handlerMessage = '';
+  roleMessage = '';
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Edit Profile',
+      buttons: [
+        {
+          text: 'Cancel',
+          // role: 'cancel',
+          handler: () => {
+            this.handlerMessage = 'Alert canceled';
+          },
+        },
+        {
+          text: 'OK',
+          role: 'Saved',
+          handler: () => {
+            this.handlerMessage = 'Alert confirmed';
+          },
+        },
+      ],
+      inputs: [
+        // {
+        //   placeholder: 'Name',
+        // },
+        {
+          type: 'textarea',
+          placeholder: 'Favorite Genres',
+        },
+        // {
+        //   type: 'number',
+        //   placeholder: 'Age',
+        //   min: 1,
+        //   max: 100,
+        // },
+        {
+          type: 'textarea',
+          placeholder: 'A little about yourself',
+        },
+      ],
+    });
+
+    await alert.present();
+    const { role } = await alert.onDidDismiss();
+    this.roleMessage = `${role}`;
+  }
+  ngOnInit() {}
 }
