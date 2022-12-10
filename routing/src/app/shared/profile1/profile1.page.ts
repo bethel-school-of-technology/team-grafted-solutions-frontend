d75e72fb73f1f94fc8bb49ec4b9a042a259d1e31
91ap <101858100+91ap@users.noreply.github.com>
2022-12-09 21:23:00 -0600

    "removed alert input"

--- routing/src/app/shared/profile1/profile1.page.ts
+++ routing/src/app/shared/profile1/profile1.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { AlertController, AlertInput } from '@ionic/angular';
 @Component({
   selector: 'app-profile1',
   templateUrl: './profile1.page.html',
@@ -15,61 +15,5 @@ export class Profile1Page implements OnInit {
     this.isModalOpen = !isOpen;
   }
 
-  constructor(private alertController: AlertController) {}
-
-  handlerMessage = '';
-  roleMessage = '';
-
-  async presentAlert() {
-    const alert = await this.alertController.create({
-      header: 'Edit Profile',
-      buttons: [
-        'Save',
-        // {
-        // text: 'Cancel',
-        // role: 'cancel',
-        // handler: () => {
-        //   this.handlerMessage = 'Alert canceled';
-        // },
-        // },
-        // {
-        // text: 'OK',
-        // role: 'Saved',
-        // handler: () => {
-        //   this.handlerMessage = 'Alert confirmed';
-        // },
-        // },
-      ],
-      inputs: [
-        // {
-        //   placeholder: 'Name',
-        // },
-        {
-          type: 'textarea',
-          placeholder: 'Favorite Genres',
-          handler: () => {
-            this.handlerMessage = 'Favorite Genres';
-          },
-        },
-        // {
-        //   type: 'number',
-        //   placeholder: 'Age',
-        //   min: 1,
-        //   max: 100,
-        // },
-        {
-          type: 'textarea',
-          placeholder: 'A little about yourself',
-          handler: () => {
-            this.handlerMessage = 'About Me:';
-          },
-        },
-      ],
-    });
-
-    await alert.present();
-    const { role } = await alert.onDidDismiss();
-    this.roleMessage = `${role}`;
-  }
   ngOnInit() {}
 }
