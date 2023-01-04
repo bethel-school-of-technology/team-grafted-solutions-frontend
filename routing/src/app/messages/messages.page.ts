476a62486ba25595d77d6a2beb39be0707b8a098
Kenny Tyson <kennytyson48@gmail.com>
2023-01-04 15:31:02 -0800

    updates to direct messages

--- routing/src/app/messages/messages.page.ts
+++ routing/src/app/messages/messages.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Route, Router } from '@angular/router';
 import { IonModal } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core';
 
@@ -15,10 +16,19 @@ selectTabs = ""
 newPost = '';
 newHeadline= '';
 post: any[] = [];
+open_new_chat = false;
+@Input() item: any;
+segment = 'chats'
+
+User = [
+  {id:1, name: 'User1', photo:'https://i.pravatar.cc/325'},
+
+]
 
 
   cancel() {
     this.modal.dismiss(null, 'cancel');
+    this.open_new_chat = false;
   }
 
   confirm() {
@@ -33,6 +43,19 @@ post: any[] = [];
     }
   }
 
+  newChat() {}
+  
+  redirect() {
 
+  }
+
+  getChat(item){
+    this.router.navigate(['/', 'socialvibez', 'messages', 'chats', item?.id])
+
+  }
+
+  constructor(
+    private router: Router
+  ) {}
   ngOnInit() {}
 }
