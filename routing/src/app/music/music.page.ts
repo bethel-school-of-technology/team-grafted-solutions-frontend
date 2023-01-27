c9561df7d4a7abc5dbeac7aa95d12e49bda27cbc
Randy Beebe <randybb27@gmail.com>
2023-01-27 00:08:09 -0600

    "fixed accessToken line 44 music.page.ts"

--- routing/src/app/music/music.page.ts
+++ routing/src/app/music/music.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
 import { Music } from '../models/music';
 import { MusicService } from '../service/music.service';
-// import SpotifyWebApi from 'spotify-web-api-js'
 import { AnimationController } from '@ionic/angular';
 import { modalController } from '@ionic/core';
 
@@ -14,15 +13,12 @@ declare var cordova: any
 })
 export class MusicPage implements OnInit {
   accessToken: any;
-
   searchToken: any;
-  // spotifyApi = new SpotifyWebApi();
   music: any[] = [];
   code: any;
   searchTerm: string = '';
   public data = [this.music];
   public results = [...this.data];
-  // loading: Loading;
   isModalOpen = false;
   presentingElement = null;
 
@@ -41,41 +37,16 @@ export class MusicPage implements OnInit {
   ngOnInit() {
     this.code = new URLSearchParams(window.location.search).get('code');
     this.getAccessToken(this.code);
-    // this.presentingElement = document.querySelector('.ion-page');
 
   }
-  
-  
-
-  // searchMusic() {
-  //   this.service.searchMusic(this.searchTerm).subscribe((m) => (this.music = m));
-  // }
-
-  // getSong() {
-  //   let token = { token: JSON.parse(this.accessToken) };
-
-  //   this.service.getSong(this.searchTerm, token).subscribe((m) => (this.music = m));
-  // }
-
-  // getArtist() {
-  //   let token = { token: JSON.parse(this.accessToken) };
-
-  //   this.service.getSong(this.searchTerm, token).subscribe((m) => (this.music = m));
-  // }
 
   searchMusicTest() {
-    let token = { token: JSON.parse(this.searchToken) };
+    let token = { token: JSON.parse(this.accessToken) };
 
     this.service.searchMusicTest(this.searchTerm, token).subscribe((m) => {
       this.music = m;
     });
   }
-
-  // getMusic() {
-  //   let token = { token: JSON.parse(this.accessToken) };
-
-  //   this.service.getMusic(this.searchTerm).subscribe((m) => (this.music = m));
-  // }
 
   sortArtist() {
     this.service.sortArtist().subscribe((m) => (this.music = m));
@@ -93,19 +64,6 @@ export class MusicPage implements OnInit {
       this.searchToken = localStorage.getItem('searchToken')
     });
   }
-
-  // getUserPlaylist(){
-  //   this.loading = this.loadingCtrl.create({
-  //     content: 'loading Playlist...'
-  //   });
-  //   this.loading.present();
-
-  //   this.spotifyApi.getUserPlaylists().then(data => {
-  //     if (this.loading) {
-  //       this.loading.dismiss();
-  //     }
-  //   })
-  // }
   getPreviewUrl() {
     let token = { token: JSON.parse(this.accessToken) };
       this.service.getPreviewUrl(this.searchTerm, token).subscribe((m) => {
@@ -120,35 +78,4 @@ export class MusicPage implements OnInit {
   setClose(isOpen: boolean) {
     this.isModalOpen = !isOpen;
   }
-
-  // enterAnimation = (baseEl: HTMLElement) => {
-  //   const root = baseEl.shadowRoot;
-
-  //   const backdropAnimation = this.animationCtrl
-  //     .create()
-  //     .addElement(root?.querySelector('ion-backdrop')!)
-  //     .fromTo('opacity', '0.01', 'var(--backdrop-opacity)');
-
-  //   const wrapperAnimation = this.animationCtrl
-  //     .create()
-  //     .addElement(root?.querySelector('.modal-wrapper')!)
-  //     .keyframes([
-  //       { offset: 0, opacity: '0', transform: 'scale(0)' },
-  //       { offset: 1, opacity: '0.99', transform: 'scale(1)' },
-  //     ]);
-
-  //   return this.animationCtrl
-  //     .create()
-  //     .addElement(baseEl)
-  //     .easing('ease-out')
-  //     .duration(500)
-  //     .addAnimation([backdropAnimation, wrapperAnimation]);
-  // };
-
-  // leaveAnimation = (baseEl: HTMLElement) => {
-  //   return this.enterAnimation(baseEl).direction('reverse');
-  // };
-
-
-
 }
