b3c68250b9940ebc6d037225d4b97370cd17a30a
91ap <101858100+91ap@users.noreply.github.com>
2023-01-29 17:46:07 -0600

    "changed some syntax"

--- routing/src/app/CRUD/crud-service.service.ts
+++ routing/src/app/CRUD/crud-service.service.ts
@@ -4,33 +4,28 @@ import { Observable, of } from 'rxjs';
 import { Message } from '../models/message';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CrudServiceService {
-baseURL: string = "http://localhost:3001/messages";
+  baseURL: string = 'http://localhost:3001/messages';
 
-
-
-  listOfMessages: Message[] = [{
-    message: "this is my newest message"
-  }];
-  title: string = "my messages";
-  constructor(
-    private http: HttpClient
-  ) { }
+  listOfMessages: Message[] = [
+    {
+      message: 'this is my newest message',
+    },
+  ];
+  title: string = 'my messages';
+  constructor(private http: HttpClient) {}
 
   // getAllMessages(): Observable<Message[]> {
   //   return of (this.listOfMessages);
   // }
 
-
   getAllMessages(): Observable<Message[]> {
     return this.http.get<Message[]>(this.baseURL);
   }
 
-  createNewMessage(newMessage: Message): Observable<Message>{
+  createNewMessage(newMessage: any): Observable<Message> {
     return this.http.post<Message>(this.baseURL, newMessage);
-}
-
-
+  }
 }
--- routing/src/app/messages/chat/chat.page.ts
+++ routing/src/app/messages/chat/chat.page.ts
@@ -7,94 +7,79 @@ import { CreateMComponent } from 'src/app/shared/create-m/create-m.component';
 import { CrudServiceService } from 'src/app/CRUD/crud-service.service';
 import { MusicService } from 'src/app/service/music.service';
 
-
-
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.page.html',
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
-
-  baseURL = "http://localhost:3001/messages" 
+  baseURL = 'http://localhost:3001/messages';
   accessToken: any;
-          //replace 'sender' w/ the users name
+  //replace 'sender' w/ the users name
   name: string = 'Sender';
-  newMessage: Message;
+  newMessage: any;
   isLoading = false;
   messageInfo: any = {
-    mInfo:" ",  
-  }
+    mInfo: ' ',
+  };
   searchToken: any;
   makeMessage: Message = new Message();
   code: any;
-  currentUserId = 1
+  currentUserId = 1;
   chats = [
     // {id: 1, sender: 1, message: 'hi how are you'},
     // {id: 2, sender: 2, message: 'hello there'},
-
   ];
   searchTerm: any;
 
-
-  constructor(
-    private c: ChatService,
-    private http: HttpClient,
-    private crud: CrudServiceService,
-    private service: MusicService,
-
-  ) {
-    this.accessToken = localStorage.getItem('accessToken');
-
-   }
+  constructor(private c: ChatService, private http: HttpClient, private crud: CrudServiceService, private service: MusicService) {
+    // this.accessToken = localStorage.getItem('accessToken');
+    let data: any = JSON.parse(localStorage.getItem('accessToken'));
+    this.accessToken = data.token;
+  }
   title: string = '';
   messageLists: Message[] = [];
 
   ngOnInit(): void {
     this.code = new URLSearchParams(window.location.search).get('code');
 
-    this.getAccessToken(this.code);
+    // this.getAccessToken(this.code);
 
     this.title = this.crud.title;
-    this.crud.getAllMessages() 
-    .subscribe(response => this.messageLists = response);  
-
+    this.crud.getAllMessages().subscribe((response) => (this.messageLists = response));
   }
 
-
-  createMessage (newMessage: Message): Observable<Message[]> {
-    console.log(newMessage)
-    return this.http.post<Message[]>(this.baseURL, newMessage)
-    
-
+  createMessage(newMessage: Message): Observable<Message[]> {
+    console.log(newMessage);
+    return this.http.post<Message[]>(this.baseURL, newMessage);
   }
 
-  createNewMessage(newMessage: Message): Observable<Message>{
+  createNewMessage(newMessage: Message): Observable<Message> {
     return this.http.post<Message>(this.baseURL, newMessage);
-}
+  }
 
-getAccessToken(code: any) {
-  this.service.getAccessToken(code).subscribe((result:any) => {
-    localStorage.setItem('accessToken', JSON.stringify(result));
-    localStorage.setItem('searchToken', JSON.stringify(result.token));
-    this.accessToken = localStorage.getItem('accessToken');
-    this.searchToken = localStorage.getItem('searchToken')
-  });
-}
+  getAccessToken(code: any) {
+    this.service.getAccessToken(code).subscribe((result: any) => {
+      localStorage.setItem('accessToken', JSON.stringify(result));
+      localStorage.setItem('searchToken', JSON.stringify(result.token));
+      this.accessToken = localStorage.getItem('accessToken');
+      this.searchToken = localStorage.getItem('searchToken');
+    });
+  }
 
+  onSubmit() {
+    // let token = { token: JSON.parse(this.accessToken) };
 
-onSubmit() {
-  
-  let token = { token: JSON.parse(this.accessToken) };
+    // // this.crud.createNewMessage(this.newMessage).subscribe(response => {
+    // //   console.log(response);
 
-  // this.crud.createNewMessage(this.newMessage).subscribe(response => {
-  //   console.log(response);
+    //   this.crud.createNewMessage(this.searchTerm).subscribe((m) => {
+    //     this.newMessage = m;
 
-    this.crud.createNewMessage(this.searchTerm).subscribe((m) => {
+    // });
+
+    this.crud.createNewMessage({ message: ' testings ', token: this.accessToken }).subscribe((m) => {
       this.newMessage = m;
-  
-
-  });
-}
-
+    });
+  }
 }
