63e3cac0dad3c10172329893ba088f2cea28f712
91ap <101858100+91ap@users.noreply.github.com>
2023-01-29 18:42:01 -0600

    "post to backend"

--- routing/src/app/messages/chat/chat.page.ts
+++ routing/src/app/messages/chat/chat.page.ts
@@ -7,47 +7,35 @@ import { CreateMComponent } from 'src/app/shared/create-m/create-m.component';
 import { CrudServiceService } from 'src/app/CRUD/crud-service.service';
 import { MusicService } from 'src/app/service/music.service';
 
-
-
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.page.html',
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
-
-  baseURL = "http://localhost:3001/messages" 
+  baseURL = 'http://localhost:3001/messages';
   accessToken: any;
-          //replace 'sender' w/ the users name
+  //replace 'sender' w/ the users name
   name: string = 'Sender';
   newMessage: any;
   isLoading = false;
   messageInfo: any = {
-    mInfo:" ",  
-  }
+    mInfo: ' ',
+  };
   searchToken: any;
   makeMessage: Message = new Message();
   code: any;
-  currentUserId = 1
+  currentUserId = 1;
   chats = [
     // {id: 1, sender: 1, message: 'hi how are you'},
     // {id: 2, sender: 2, message: 'hello there'},
-
   ];
   searchTerm: any;
-
-
-  constructor(
-    private c: ChatService,
-    private http: HttpClient,
-    private crud: CrudServiceService,
-    private service: MusicService,
-
-  ) {
+  mySubscription: any;
+  constructor(private c: ChatService, private http: HttpClient, private crud: CrudServiceService, private service: MusicService) {
     let data: any = JSON.parse(localStorage.getItem('accessToken'));
     this.accessToken = data.token;
-
-   }
+  }
   title: string = '';
   messageLists: Message[] = [];
 
@@ -57,47 +45,41 @@ export class ChatPage implements OnInit {
     // this.getAccessToken(this.code);
 
     this.title = this.crud.title;
-    this.crud.getAllMessages() 
-    .subscribe(response => this.messageLists = response);  
-
+    this.crud.getAllMessages().subscribe((response) => (this.messageLists = response));
   }
 
-
-  createMessage (newMessage: Message): Observable<Message[]> {
-    console.log(newMessage)
-    return this.http.post<Message[]>(this.baseURL, newMessage)
-    
-
+  createMessage(newMessage: Message): Observable<Message[]> {
+    // console.log(newMessage);
+    return this.http.post<Message[]>(this.baseURL, newMessage);
   }
 
-  createNewMessage(newMessage: Message): Observable<Message>{
+  createNewMessage(newMessage: Message): Observable<Message> {
+    // console.log(this.newMessage);
+
     return this.http.post<Message>(this.baseURL, newMessage);
-}
+  }
 
-getAccessToken(code: any) {
-  this.service.getAccessToken(code).subscribe((result:any) => {
-    localStorage.setItem('accessToken', JSON.stringify(result));
-    localStorage.setItem('searchToken', JSON.stringify(result.token));
-    this.accessToken = localStorage.getItem('accessToken');
-    this.searchToken = localStorage.getItem('searchToken')
-  });
-}
+  getAccessToken(code: any) {
+    this.service.getAccessToken(code).subscribe((result: any) => {
+      localStorage.setItem('accessToken', JSON.stringify(result));
+      localStorage.setItem('searchToken', JSON.stringify(result.token));
+      this.accessToken = localStorage.getItem('accessToken');
+      this.searchToken = localStorage.getItem('searchToken');
+    });
+  }
 
+  onSubmit() {
+    // let token = { token: JSON.parse(this.accessToken) };
 
-onSubmit() {
-  
-  // let token = { token: JSON.parse(this.accessToken) };
+    // this.crud.createNewMessage(this.newMessage).subscribe(response => {
+    //   console.log(response);
 
-  // this.crud.createNewMessage(this.newMessage).subscribe(response => {
-  //   console.log(response);
+    // this.searchTerm
 
-  // this.searchTerm
-
-  this.crud.createNewMessage({message: this.searchTerm, token: this.accessToken }).subscribe((m) => {
-    this.newMessage = m;
-
-
-});
-}
-
+    this.crud.createNewMessage({ message: this.messageInfo, token: this.accessToken }).subscribe((m) => {
+      this.newMessage = m;
+      console.log(this.messageInfo);
+      this.ngOnInit();
+    });
+  }
 }
