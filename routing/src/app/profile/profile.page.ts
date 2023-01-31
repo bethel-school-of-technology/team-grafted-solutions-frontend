67127ac31b6f6fb0177a457cd015896bb029c73b
91ap <101858100+91ap@users.noreply.github.com>
2023-01-31 01:11:30 -0600

    "minor updates"

--- routing/src/app/profile/profile.page.ts
+++ routing/src/app/profile/profile.page.ts
@@ -7,7 +7,6 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-  isModalOpen = false;
   @Input()
   public user: any[] = [];
   @Input()
@@ -16,32 +15,11 @@ export class ProfilePage implements OnInit {
   accessToken: any;
 
   likedSongs() {}
-  setOpen(isOpen: boolean) {
-    this.isModalOpen = isOpen;
-  }
-  setClose(isOpen: boolean) {
-    this.isModalOpen = !isOpen;
-  }
-  message = '';
-  bio = '';
+
   constructor(private modalCtrl: ModalController) {}
 
-  async openModal() {
-    const modal = await this.modalCtrl.create({
-      component: ProfilePage,
-    });
-    modal.present();
-
-    const { data, role } = await modal.onWillDismiss();
-
-    if (role === 'confirm') {
-      this.message = `${data}!`;
-      this.bio = `${data}`;
-    }
-  }
   ngOnInit() {
-    // let data = { token: JSON.parse(this.accessToken) };
-    // let data = JSON.parse(localStorage.getItem('accessToken'))
+    // let data = JSON.parse(localStorage.getItem('accessToken'));
     // this.userData = data.userData;
     // console.log(this.userData);
     // this.likedSongs();
