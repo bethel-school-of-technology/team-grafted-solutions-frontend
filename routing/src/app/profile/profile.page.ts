6a7cff26f2ccaab3727eddd2f164b0a4124b37b7
91ap <101858100+91ap@users.noreply.github.com>
2023-02-02 02:19:11 -0600

    "cleaning code"

--- routing/src/app/profile/profile.page.ts
+++ routing/src/app/profile/profile.page.ts
@@ -16,34 +16,12 @@ export class ProfilePage implements OnInit {
   accessToken: any;
 
   likedSongs() {}
-  setOpen(isOpen: boolean) {
-    this.isModalOpen = isOpen;
-  }
-  setClose(isOpen: boolean) {
-    this.isModalOpen = !isOpen;
-  }
-  message = '';
-  bio = '';
+
   constructor(private modalCtrl: ModalController) {}
 
-  async openModal() {
-    const modal = await this.modalCtrl.create({
-      component: ProfilePage,
-    });
-    modal.present();
-
-    const { data, role } = await modal.onWillDismiss();
-
-    if (role === 'confirm') {
-      this.message = `${data}!`;
-      this.bio = `${data}`;
-    }
-  }
   ngOnInit() {
-    // let data = { token: JSON.parse(this.accessToken) };
-    // let data = JSON.parse(localStorage.getItem('accessToken'))
-    // this.userData = data.userData;
-    // console.log(this.userData);
-    // this.likedSongs();
+    let data = JSON.parse(localStorage.getItem('accessToken')!);
+    this.userData = data.userData;
+    console.log(this.userData);
   }
 }
